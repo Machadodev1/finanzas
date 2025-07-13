378cda8f09bb1dea8e3163a0704d74c4e07a47d4
Machadodev1 <machadoandres444@gmail.com>
2025-07-13 17:03:57 -0500

    cambios minimos

--- finanzas.py
+++ finanzas.py
@@ -1,7 +1,6 @@
 import time
 import datetime
 import os
-hora = datetime.datetime.now().strftime("%Y-%m-%d")
 def crear_meta():
     nombre_meta = input("Ingrese el nombre de la meta a alcanzar: ")
     try:
@@ -13,7 +12,7 @@ def crear_meta():
         archivo.write(f"META: {nombre_meta} | {valor_meta}\n")
     print(f"la meta fue guardada exitosamente.")
 
-def registrar_aporte(monto):
+def registrar_aporte():
     nombre_meta = input("Ingrese el nombre de la meta a la que desea aportar: ")
     existe = os.path.exists(nombre_meta)
     if existe == True:
@@ -28,3 +27,30 @@ def registrar_aporte(monto):
         except:
             print("Monto invalido...")
             return
+    else:
+        print("Esta meta no existe...")
+        return
+
+def ver_estado():
+    nombre_meta = input("Ingrese el nombre de la meta que desea ver sus detalles: ")
+    existe = os.path.exists(nombre_meta)
+    if existe:
+        with open(f"{nombre_meta}", "r") as archivo:
+            lineas = archivo.readlines()
+            meta_linea = lineas[0].strip()
+            _, valor_str = meta_linea.split("|")
+            valor_meta = int(valor_str.strip())
+            total_aportado = 0
+            for linea in lineas[1:]:
+                try:
+                    _, monto = linea.strip().split("|")
+                    total_aportado += int(monto.strip())
+                except:
+                    pass
+        
+        print(f"\n📋 Meta: {nombre_meta}")
+        print(f"🎯 Valor de la meta: {valor_meta}")
+        print(f"💰 Total aportado: {total_aportado}")
+        print(f"🔻 Restante: {valor_meta - total_aportado}\n")
+    else:
+        print("❌ Esta meta no existe.")
---
+++ main.py
@@ -0,0 +1,4 @@
+from finanzas import ver_estado
+from finanzas import registrar_aporte
+from finanzas import crear_meta
+ver_estado()
